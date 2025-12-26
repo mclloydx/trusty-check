dad33a8921137e796ed167b50c7694076f4fab0b
themclloyd <themclloyd@gmail.com>
2025-12-26 22:39:39 +0200

    update

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -213,3 +213,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   );
 }
 
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
+
